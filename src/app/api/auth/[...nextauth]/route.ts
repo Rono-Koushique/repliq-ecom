d2fb8c3bd6052ea7e0888bbb527b885c30164ac0
Rono-Koushique <rono2170@gmail.com>
2023-05-26 21:54:18 +0600

    url

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -36,7 +36,7 @@ const handler = NextAuth({
     pages: {
         signIn: "/auth/login",
     },
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
